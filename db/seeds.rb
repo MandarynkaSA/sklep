a93e59f2f35a44db7fa42db48e6f3b0dc406e1bb
dkorybalski <dawid.korybalski@gmail.com>
2019-10-23 08:12:31 +0200

    Poprawiono działanie skryptu dodającego rekordy do bazy dancyh i dodano folder do .gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,20 @@
 8.times do
     Category.create! name: Faker::Hacker.ingverb.capitalize
-  end
+end
   
-  category_ids = Category.pluck(:id)
+category_ids = Category.pluck(:id)
   
-  print "Product "
-  200.times do
+print "Product "
+200.times do
     product = Product.create! name: "#{Faker::Hacker.verb} #{Faker::Hacker.noun}".capitalize,
-      description: Faker::Hacker.say_something_smart,
-      long_description: Faker::Lorem.paragraphs(3).join("\n\n"),
-      category_id: category_ids.sample,
-      price: Faker::Number.decimal(3, 2)
+        description: Faker::Hacker.say_something_smart,
+        log_description: Faker::Lorem.paragraphs(number: 3).join("\n\n"),
+        category_id: category_ids.sample,
+        price: Faker::Number.decimal(l_digits: 3, r_digits: 2)
     product.remote_photo_url = Faker::Avatar.image(
-      product.name.parameterize,
-      "640x480",
-      "jpg",
-      "set#{[1, 2, 3].sample}",
-      "bg#{[1, 2].sample}"
+      slug: product.name.parameterize, size: "640x480", format: "jpg", set: "set#{[1, 2, 3].sample}", bgset: "bg#{[1, 2].sample}"
     )
     product.save
     print "."
-  end
-  puts
+end
+puts
